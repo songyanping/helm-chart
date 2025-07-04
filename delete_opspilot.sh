f7a60b316a6c0ab5a2892c7c96c3534e085ee0fe
Even <songyanping@fulan.com.cn>
2025-07-04 12:18:40 +0800

    update

--- delete_opspilot.sh
+++ delete_opspilot.sh
@@ -7,51 +7,43 @@ NAMESPACE="opspilot"
 helm uninstall watch -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "卸载 watch 失败"
-    exit 1
 fi
 
 helm uninstall console -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "卸载 console 失败"
-    exit 1
 fi
 
 helm uninstall lookout -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "卸载 lookout 失败"
-    exit 1
 fi
 
 # 卸载中间件服务
 helm uninstall elasticsearch -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "卸载 elasticsearch 失败"
-    exit 1
 fi
 
 helm uninstall kibana -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "卸载 kibana 失败"
-    exit 1
 fi
 
 helm uninstall redis -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "卸载 redis 失败"
-    exit 1
 fi
 
 helm uninstall prometheus -n $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "卸载 prometheus 失败"
-    exit 1
 fi
 
 # 删除命名空间（根据需要可选）
 kubectl delete namespace $NAMESPACE
 if [ $? -ne 0 ]; then
     echo "删除命名空间 $NAMESPACE 失败"
-    exit 1
 fi
 
 echo "所有组件已成功卸载并清理完成"
