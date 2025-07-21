e8343cedf073c2b93891c2864782862506a4ae9d
Even <songyanping@fulan.com.cn>
2025-07-21 11:24:54 +0800

    add datasource

---
+++ insert_datasource.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Elasticsearch 地址和索引名称
+ES_URL="http://localhost:9200"
+INDEX_NAME="manage_zone"
+
+# JSON 数据
+JSON_DATA='{
+  "id": "opspilot-ft2",
+  "name": "Opspilot Ft2",
+  "app": "opspilot",
+  "env": "ft2",
+  "delayedMin": 0,
+  "serviceBlackList": [],
+  "apiBlackList": [],
+  "exceptionBlackList": [],
+  "sort": 99,
+  "datasource": {
+    "apm": {
+      "type": "skywalking",
+      "enable": false,
+      "app": "",
+      "env": "",
+      "endpoint": "http://skywalking-oap.sre-base.svc.cluster.local:12800/graphql",
+      "endpoint2": "http://10.158.215.42:9200",
+      "group": ["ft2@opspilot"],
+      "mode": "full",
+      "traceAnalysis": true,
+      "interval": 60,
+      "concurrency": 10,
+      "ak": "",
+      "sk": ""
+    },
+    "frontend": {
+      "type": "skywalking",
+      "enable": false,
+      "app": "",
+      "env": "",
+      "endpoint": "http://skywalking-oap.sre-base.svc.cluster.local:12800/graphql",
+      "endpoint2": "http://10.158.215.42:9200",
+      "group": ["ft2"],
+      "mode": "full",
+      "traceAnalysis": true,
+      "ak": "",
+      "sk": ""
+    }
+  }
+}'
+
+# 发送请求
+curl -XPOST "$ES_URL/$INDEX_NAME/_doc" \
+  -H "Content-Type: application/json" \
+  -d "$JSON_DATA"
